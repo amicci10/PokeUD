bd7c1996d97cf383c82ae40e195342f358bfe792
amicci10 <henrique.amicci@hotmail.com>
2023-06-13 09:49:34 -0300

    Versão 1.0

---
+++ modules/rc_sound/rcsound.lua
@@ -0,0 +1,228 @@
+SOUNDS_CONFIG = {
+	soundChannel = SoundChannels.Music,
+	checkInterval = 500,
+	folder = 'music/',
+	noSound = 'Nenhum arquivo de som para esta area',
+}
+
+SOUNDS = {
+	-- Skumdush Boeiros
+
+{fromPos = {x = 881, y = 1020, z = 8}, toPos = {x = 918, y = 1038, z = 8}, sound = "saffron.ogg"},
+	{fromPos = {x = 1040, y = 1039, z = 7}, toPos = {x = 1056, y = 1057, z = 7}, sound = "saffron.ogg"},
+{fromPos = {x = 1040, y = 1039, z = 6}, toPos = {x = 1056, y = 1057, z = 6}, sound = "saffron.ogg"},
+{fromPos = {x = 1040, y = 1039, z = 5}, toPos = {x = 1056, y = 1057, z = 5}, sound = "saffron.ogg"},
+        {fromPos = {x = 42, y = 42, z = 7}, toPos = {x = 57, y = 57, z = 7}, sound = "abertura.ogg"},
+        
+        {fromPos = {x = 970, y = 946, z = 7}, toPos = {x = 1301, y = 1007, z = 7}, sound = "gotpoke.ogg"},
+        {fromPos = {x = 1005, y = 1005, z = 7}, toPos = {x = 1055, y = 1022, z = 7}, sound = "gotpoke.ogg"},
+{fromPos = {x = 943, y = 1111, z = 7}, toPos = {x = 1180, y = 1203, z = 7}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1062, y = 1006, z = 7}, toPos = {x = 1079, y = 1012, z = 7}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1054, y = 1008, z = 7}, toPos = {x = 1061, y = 1014, z = 7}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1102, y = 801, z = 7}, toPos = {x = 1309, y = 1004, z = 7}, sound = "gotpoke.ogg"},
+
+{fromPos = {x = 970, y = 946, z = 6}, toPos = {x = 1301, y = 1007, z = 6}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1005, y = 1005, z = 6}, toPos = {x = 1055, y = 1022, z = 6}, sound = "gotpoke.ogg"},
+{fromPos = {x = 943, y = 1111, z = 6}, toPos = {x = 1180, y = 1203, z = 6}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1062, y = 1006, z = 6}, toPos = {x = 1079, y = 1012, z = 6}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1054, y = 1008, z = 6}, toPos = {x = 1061, y = 1014, z = 6}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1102, y = 801, z = 6}, toPos = {x = 1309, y = 1004, z = 6}, sound = "gotpoke.ogg"},
+
+
+{fromPos = {x = 970, y = 946, z = 5}, toPos = {x = 1301, y = 1007, z = 5}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1005, y = 1005, z = 5}, toPos = {x = 1055, y = 1022, z = 5}, sound = "gotpoke.ogg"},
+{fromPos = {x = 943, y = 1111, z = 5}, toPos = {x = 1180, y = 1203, z = 5}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1062, y = 1006, z = 5}, toPos = {x = 1079, y = 1012, z = 5}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1054, y = 1008, z = 5}, toPos = {x = 1061, y = 1014, z = 5}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1102, y = 801, z = 5}, toPos = {x = 1309, y = 1004, z = 5}, sound = "gotpoke.ogg"},
+
+
+{fromPos = {x = 970, y = 946, z = 4}, toPos = {x = 1301, y = 1007, z = 4}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1005, y = 1005, z = 4}, toPos = {x = 1055, y = 1022, z = 4}, sound = "gotpoke.ogg"},
+{fromPos = {x = 943, y = 1111, z = 4}, toPos = {x = 1180, y = 1203, z = 4}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1062, y = 1006, z = 4}, toPos = {x = 1079, y = 1012, z = 4}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1054, y = 1008, z = 4}, toPos = {x = 1061, y = 1014, z = 4}, sound = "gotpoke.ogg"},
+{fromPos = {x = 1102, y = 801, z = 4}, toPos = {x = 1309, y = 1004, z = 4}, sound = "gotpoke.ogg"},
+	
+
+-- Batle
+
+{fromPos = {x = 1011, y = 1032, z = 7}, toPos = {x = 1019, y = 1051, z = 7}, sound = "battle.ogg"},
+	
+	-- Cidade --
+	
+		{fromPos = {x = 1020, y = 1030, z = 7}, toPos = {x = 1039, y = 1098, z = 7}, sound = "cidade.ogg"},
+                {fromPos = {x = 1041, y = 1058, z = 7}, toPos = {x = 1076, y = 1109, z = 7}, sound = "cidade.ogg"},
+                {fromPos = {x = 1056, y = 1015, z = 7}, toPos = {x = 1113, y = 1066, z = 7}, sound = "cidade.ogg"},
+                {fromPos = {x = 943, y = 1111, z = 7}, toPos = {x = 1180, y = 1203, z = 7}, sound = "cidade.ogg"},
+                {fromPos = {x = 1028, y = 1036, z = 7}, toPos = {x = 1058, y = 1039, z = 7}, sound = "cidade.ogg"},
+	
+	
+	-- Fly --
+	
+		{fromPos = {x = 54, y = 289, z = 0}, toPos = {x = 2013, y = 1202, z = 0}, sound = "ventania.ogg"},
+		
+	
+		-- Pvp
+		{fromPos = {x = 934, y = 1032, z = 13}, toPos = {x = 1011, y = 1094, z = 13}, priority = 1, sound="pvp.ogg"},
+                {fromPos = {x = 1015, y = 1049, z = 13}, toPos = {x = 1036, y = 1077, z = 13}, priority = 1, sound="pvp.ogg"},
+		
+	
+	-- Mewtwo quest
+
+{fromPos = {x = 218, y = 1151, z = 13}, toPos = {x = 244, y = 1177, z = 13}, priority = 1, sound="mewtwo.ogg"},
+{fromPos = {x = 244, y = 1132, z = 13}, toPos = {x = 531, y = 1229, z = 13}, priority = 1, sound="fundo11.ogg"},
+{fromPos = {x = 478, y = 1146, z = 12}, toPos = {x = 619, y = 1343, z = 12}, priority = 1, sound="fundo6.ogg"},
+{fromPos = {x = 503, y = 1329, z = 11}, toPos = {x = 596, y = 1450, z = 11}, priority = 1, sound="fundo6.ogg"},
+
+	
+		-- Dragon
+		{fromPos = {x=1089, y=1110, z=8}, toPos = {x=1135, y=1142, z=8}, priority = 1, sound="Fairy Tail - Main.ogg"},
+		{fromPos = {x=1102, y=1128, z=9}, toPos = {x=1131, y=1149, z=9}, priority = 1, sound="Fairy Tail - Main.ogg"},
+		{fromPos = {x=1117, y=1124, z=9}, toPos = {x=1128, y=1127, z=9}, priority = 1, sound="Fairy Tail - Main.ogg"},
+		{fromPos = {x=1100, y=1104, z=10}, toPos = {x=1127, y=1131, z=10}, priority = 1, sound="Fairy Tail - Main.ogg"},
+		{fromPos = {x=1103, y=1108, z=9}, toPos = {x=1108, y=1112, z=9}, priority = 1, sound="Fairy Tail - Main.ogg"},
+
+		{fromPos = {x=1096, y=1088, z=5}, toPos = {x=1110, y=1100, z=6}, priority = 1, sound="Fairy Tail - Main.ogg"},
+
+		
+		
+} ----------
+
+-- Sound
+local rcSoundChannel
+local showPosEvent
+local playingSound
+
+-- Design
+soundWindow = nil
+soundButton = nil
+
+function toggle()
+  if soundButton:isOn() then
+    soundWindow:close()
+    soundButton:setOn(false)
+  else
+    soundWindow:open()
+    soundButton:setOn(true)
+  end
+end
+
+function onMiniWindowClose()
+  soundButton:setOn(false)
+end
+
+function init()
+	for i = 1, #SOUNDS do
+		SOUNDS[i].sound = SOUNDS_CONFIG.folder .. SOUNDS[i].sound
+	end
+	
+	connect(g_game, { onGameStart = onGameStart,
+                    onGameEnd = onGameEnd })
+	
+	rcSoundChannel = g_sounds.getChannel(SOUNDS_CONFIG.soundChannel)
+	-- rcSoundChannel:setGain(value/COUNDS_CONFIG.volume)
+
+	soundButton = modules.client_topmenu.addRightGameToggleButton('soundButton', tr('Sound Info') .. '', '/images/audio', toggle)
+	soundButton:setOn(true)
+	
+	soundWindow = g_ui.loadUI('rcsound', modules.game_interface.getRightPanel())
+	soundWindow:disableResize()
+	soundWindow:setup()
+	
+	if(g_game.isOnline()) then
+		onGameStart()
+	end
+end
+
+function terminate()
+	disconnect(g_game, { onGameStart = onGameStart,
+                       onGameEnd = onGameEnd })
+	onGameEnd()
+	soundWindow:destroy()
+	soundButton:destroy()
+end
+
+function onGameStart()
+	stopSound()
+	toggleSoundEvent = addEvent(toggleSound, SOUNDS_CONFIG.checkInterval)
+end
+
+function onGameEnd()
+	stopSound()
+	removeEvent(toggleSoundEvent)
+end
+
+function isInPos(pos, fromPos, toPos)
+	return
+		pos.x>=fromPos.x and
+		pos.y>=fromPos.y and
+		pos.z>=fromPos.z and
+		pos.x<=toPos.x and
+		pos.y<=toPos.y and
+		pos.z<=toPos.z
+end
+
+function toggleSound()
+	local player = g_game.getLocalPlayer()
+	if not player then return end
+	
+	local pos = player:getPosition()
+	local toPlay = nil
+
+	for i = 1, #SOUNDS do
+		if(isInPos(pos, SOUNDS[i].fromPos, SOUNDS[i].toPos)) then
+			if(toPlay) then
+				toPlay.priority = toPlay.priority or 0
+				if((toPlay.sound~=SOUNDS[i].sound) and (SOUNDS[i].priority>toPlay.priority)) then
+					toPlay = SOUNDS[i]
+				end
+			else
+				toPlay = SOUNDS[i]
+			end
+		end
+	end
+
+	playingSound = playingSound or {sound='', priority=0}
+	
+	if(toPlay~=nil and playingSound.sound~=toPlay.sound) then
+		g_logger.info("RC Sounds: New sound area detected:")
+		g_logger.info("  Position: {x=" .. pos.x .. ", y=" .. pos.y .. ", z=" .. pos.z .. "}")
+		g_logger.info("  Music: " .. toPlay.sound)
+		stopSound()
+		playSound(toPlay.sound)
+		playingSound = toPlay
+	elseif(toPlay==nil) and (playingSound.sound~='') then
+		g_logger.info("RC Sounds: New sound area detected:")
+		g_logger.info("  Left music area.")
+		stopSound()
+	end
+
+	toggleSoundEvent = scheduleEvent(toggleSound, SOUNDS_CONFIG.checkInterval)
+end
+
+function playSound(sound)
+	rcSoundChannel:enqueue(sound, 0)
+	setLabel(clearName(sound))
+end
+
+function clearName(soundName)
+	local explode = string.explode(soundName, "/")
+	soundName = explode[#explode]
+	explode = string.explode(soundName, ".ogg")
+	soundName = ''
+	for i = 1, #explode-1 do
+		soundName = soundName .. explode[i]
+	end
+	return soundName
+end
+
+function stopSound()
+	setLabel(SOUNDS_CONFIG.noSound)
+	rcSoundChannel:stop()
+	playingSound = nil
+end
+
+function setLabel(str)
+	soundWindow:recursiveGetChildById('currentSound'):getChildById('value'):setText(str)
+end
